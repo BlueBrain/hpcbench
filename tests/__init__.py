50220d4b3ce1b4174ca029b6a8e9d31969f9eb9b
Tristan Carel <tristan.carel@epfl.ch>
2017-08-03 14:24:18 +0200

    Refactor unit-tests to prepare testing of additional execution layer

---
+++ tests/__init__.py
@@ -0,0 +1,114 @@
+from textwrap import dedent
+import inspect
+import os.path as osp
+import shutil
+import sys
+import tempfile
+import unittest
+
+from cached_property import cached_property
+
+from hpcbench.api import (
+    Benchmark,
+    Metric,
+    MetricsExtractor,
+)
+from hpcbench.cli import bensh
+from hpcbench.toolbox.contextlib_ext import pushd
+
+
+class DriverTestCase(object):
+    @classmethod
+    def get_campaign_file(cls):
+        return osp.splitext(inspect.getfile(cls))[0] + '.yaml'
+
+    @classmethod
+    def setUpClass(cls):
+        cls.TEST_DIR = tempfile.mkdtemp(prefix='hpcbench-ut')
+        with pushd(cls.TEST_DIR):
+            cls.driver = bensh.main(cls.get_campaign_file())
+        cls.CAMPAIGN_PATH = osp.join(cls.TEST_DIR,
+                                     cls.driver.campaign_path)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+        #shutil.rmtree(cls.TEST_DIR)
+
+
+class FakeExtractor(MetricsExtractor):
+    @property
+    def metrics(self):
+        return dict(
+            performance=Metric('m', float),
+            standard_error=Metric('m', float)
+        )
+
+    def extract(self, outdir, metas):
+        with open(self.stdout(outdir)) as istr:
+            content = istr.readlines()
+            return dict(
+                performance=float(content[0].strip()),
+                standard_error=float(content[1].strip())
+            )
+        assert not osp.isfile(self.stderr(outdir))
+
+
+class FakeBenchmark(Benchmark):
+    name = 'fake'
+
+    description = '''
+        fake benchmark for HPCBench testing purpose
+    '''
+
+    def pre_execute(self):
+        with open('test.py', 'w') as ostr:
+            ostr.write(dedent("""\
+            from __future__ import print_function
+            import sys
+
+            print(sys.argv[1])
+            print(float(sys.argv[1]) / 10)
+            """))
+
+    @cached_property
+    def execution_matrix(self):
+        return [dict(
+            category='main',
+            command=[
+                sys.executable, 'test.py', str(value)
+            ],
+            metas=dict(
+                field=value / 10
+            )
+        )
+            for value in (10, 50, 100)
+        ]
+
+    @property
+    def metrics_extractors(self):
+        return dict(main=FakeExtractor())
+
+    @property
+    def plots(self):
+        return dict(
+            main=[
+                dict(
+                    name="{hostname} {category} Performance",
+                    series=dict(
+                        metas=['field'],
+                        metrics=[
+                            'main__performance',
+                            'main__standard_error'
+                        ],
+                    ),
+                    plotter=self.plot_performance
+                )
+            ]
+        )
+
+    def plot_performance(self, plt, description, metas, metrics):
+        plt.errorbar(metas['field'],
+                     metrics['main__performance'],
+                     yerr=metrics['main__standard_error'],
+                     fmt='o', ecolor='g', capthick=2)
--- tests/test_driver.py
+++ tests/test_driver.py
@@ -27,105 +27,9 @@ from hpcbench.cli import (
 )
 
 from .benchmark.benchmark import AbstractBenchmarkTest
+from . import DriverTestCase, FakeBenchmark
 
-
-class FakeExtractor(MetricsExtractor):
-    @property
-    def metrics(self):
-        return dict(
-            performance=Metric('m', float),
-            standard_error=Metric('m', float)
-        )
-
-    def extract(self, outdir, metas):
-        with open(self.stdout(outdir)) as istr:
-            content = istr.readlines()
-            return dict(
-                performance=float(content[0].strip()),
-                standard_error=float(content[1].strip())
-            )
-        assert not osp.isfile(self.stderr(outdir))
-
-
-class FakeBenchmark(Benchmark):
-    name = 'fake'
-
-    description = '''
-        fake benchmark for HPCBench testing purpose
-    '''
-
-    def pre_execute(self):
-        with open('test.py', 'w') as ostr:
-            ostr.write(dedent("""\
-            from __future__ import print_function
-            import sys
-
-            print(sys.argv[1])
-            print(float(sys.argv[1]) / 10)
-            """))
-
-    @cached_property
-    def execution_matrix(self):
-        return [dict(
-            category='main',
-            command=[
-                sys.executable, 'test.py', str(value)
-            ],
-            metas=dict(
-                field=value / 10
-            )
-        )
-            for value in (10, 50, 100)
-        ]
-
-    @property
-    def metrics_extractors(self):
-        return dict(main=FakeExtractor())
-
-    @property
-    def plots(self):
-        return dict(
-            main=[
-                dict(
-                    name="{hostname} {category} Performance",
-                    series=dict(
-                        metas=['field'],
-                        metrics=[
-                            'main__performance',
-                            'main__standard_error'
-                        ],
-                    ),
-                    plotter=self.plot_performance
-                )
-            ]
-        )
-
-    def plot_performance(self, plt, description, metas, metrics):
-        plt.errorbar(metas['field'],
-                     metrics['main__performance'],
-                     yerr=metrics['main__standard_error'],
-                     fmt='o', ecolor='g', capthick=2)
-
-
-class TestFakeBenchmark(AbstractBenchmarkTest, unittest.TestCase):
-    def get_benchmark_clazz(self):
-        return FakeBenchmark
-
-    def get_expected_metrics(self, category):
-        return dict(
-            performance=10.0,
-            standard_error=1.0
-        )
-
-    def get_benchmark_categories(self):
-        return ['main']
-
-
-class TestDriver(unittest.TestCase):
-    @staticmethod
-    def get_campaign_file():
-        return osp.splitext(__file__)[0] + '.yaml'
-
+class TestDriver(DriverTestCase, unittest.TestCase):
     def test_get_unknown_benchmark_class(self):
         with self.assertRaises(NameError) as exc:
             Benchmark.get_subclass('unkn0wnb3nchm4rk')
@@ -133,14 +37,6 @@ class TestDriver(unittest.TestCase):
             str(exc.exception),
             "Not a valid Benchmark class: unkn0wnb3nchm4rk"
         )
-
-    @classmethod
-    def setUpClass(cls):
-        cls.TEST_DIR = tempfile.mkdtemp()
-        with pushd(TestDriver.TEST_DIR):
-            cls.driver = bensh.main(cls.get_campaign_file())
-        cls.CAMPAIGN_PATH = osp.join(TestDriver.TEST_DIR,
-                                     cls.driver.campaign_path)
 
     def test_run_01(self):
         self.assertTrue(osp.isdir(self.CAMPAIGN_PATH))
@@ -202,6 +98,16 @@ class TestDriver(unittest.TestCase):
             # Cleanup
             exporter.remove_index()
 
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls.TEST_DIR)
+
+class TestFakeBenchmark(AbstractBenchmarkTest, unittest.TestCase):
+    def get_benchmark_clazz(self):
+        return FakeBenchmark
+
+    def get_expected_metrics(self, category):
+        return dict(
+            performance=10.0,
+            standard_error=1.0
+        )
+
+    def get_benchmark_categories(self):
+        return ['main']
