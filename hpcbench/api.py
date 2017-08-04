32233b7fc56922c8e9273d76fa15d8eefb0656f6
Tristan Carel <tristan.carel@epfl.ch>
2017-08-04 09:02:31 +0200

    Fix sphinx documentation of API

--- hpcbench/api.py
+++ hpcbench/api.py
@@ -35,8 +35,8 @@ class MetricsExtractor(with_metaclass(ABCMeta, object)):
         """List of exported metrics
 
         :return: exported metrics
-        :rtype: dictionary of
-        ``metric_name: dict('type'=python_type, 'unit'=string)``
+        :rtype: ``dict of
+                metric_name: dict('type'=python_type, 'unit'=string)``
 
         for instance:
 
@@ -53,10 +53,10 @@ class MetricsExtractor(with_metaclass(ABCMeta, object)):
     def extract(self, outdir, metas):
         """Extract metrics from benchmark output
 
-        :return: dictionary of ``metric_name: metric_value``
-        metric_value type should be the one specified in
-        the ``metrics`` member funtion.
+        :return: ``dict of metric_name: metric_value``
 
+        ``metric_value`` type should be the one specified in
+        the ``metrics`` member funtion.
         """
         raise NotImplementedError  # pragma: no cover
 
@@ -91,6 +91,7 @@ class Benchmark(with_metaclass(ABCMeta, object)):
     @abstractproperty
     def name(self):
         """Get benchmark name
+
         :rtype: string
         """
         raise NotImplementedError  # pragma: no cover
@@ -98,6 +99,7 @@ class Benchmark(with_metaclass(ABCMeta, object)):
     @abstractproperty
     def description(self):
         """Get benchmark long description
+
         :rtype: string
         """
         raise NotImplementedError  # pragma: no cover
@@ -110,31 +112,30 @@ class Benchmark(with_metaclass(ABCMeta, object)):
     def execution_matrix(self):
         """Describe benchmark commands
 
-        Provides the list of commands to perform. Every returned command
-        is a dict providing the following keys:
+        Provides list of commands to perform. Every returned command
+        is a dictionary containing the following keys:
 
-        command:
-            list of string. It contains the command to execute.
-        category:
+        * *command*:
+            a list of string containing the command to execute.
+        * category*:
             a string used to group commands together.
-        metas:
+            Most benchmark class may only need one category.
+        * *metas*:
             a dictionary providing relevant information regarding the
             executed command that may be useful afterward. Typically,
-            those are  command's inputs.
-        outputs:
-            The kind of raw data written by the command.
-            The values must match
-            Type can be a string or a list of string.
-        environment (optional):
+            those are command's inputs.
+        * *environment* (optional):
             a dictionary providing additional environment variables
             to be given to the executed command
-        srun_options (optional):
+        * *srun_options* (optional):
             When the `srun` execution layer is enabled,
             a list of string providing additional options given to
             the `srun` command.
-        srun_nodes (optional):
+        * *srun_nodes* (optional):
             When the `srun` execution layer is enabled,
             an integer providing the number of required nodes.
+            Must be greater than 0 and less than the number of nodes
+            of the tag the benchmark is part of.
 
         Execution context: for every command, a dedicated output directory
         is created and the current working directory changed to this directory
@@ -145,18 +146,18 @@ class Benchmark(with_metaclass(ABCMeta, object)):
         by the output directory.
 
         :return: commands to execute
-        :rtype: list of dictionary. For instance:
+        :rtype: list of dict. For instance:
 
         >>> def execution_matrix(self):
             for core in [1, 4, 16, 64]:
                 yield dict(
                     category='foo',
-                    command=['foo', '--cores', cores],
+                    command=['foo', '--cores', str(cores)],
                     metas=dict(cores=cores),
                 )
                 yield dict(
                     category='bar',
-                    command=['bar', '--cores', cores],
+                    command=['bar', '--cores', str(cores)],
                     metas=dict(cores=cores),
                 )
         """
@@ -174,11 +175,12 @@ class Benchmark(with_metaclass(ABCMeta, object)):
         benchmark commands.
 
         Provides metrics extractors for every categories specified
-        in the execution_matrix member method.
+        in the ``execution_matrix`` member method.
 
         :return: metrics_extractors instances for each category
-        :rtype: ``dict of list of hpcbench.api.MetricsExtractor``.
-        The list structure can be skiped when there is one
+        :rtype: ``dict of list of hpcbench.api.MetricsExtractor``
+
+        The list structure can be skipped when there is one
         element. For instance:
 
         >>> def metrics_extractors(self):
@@ -194,21 +196,24 @@ class Benchmark(with_metaclass(ABCMeta, object)):
         """Describe figures to generate
 
         :return: figure descriptions of every category
-        :rtype: dictionary of string (category) -> list of dict (description)
-        A figure description is a dictionary made of the following keys:
+        :rtype: ``dict of string -> list of dict``
 
-        name:
-            string providing figure's name
-        series:
-            dictionary describing data required to draw the figure,
-            made of 2 keys:
-            metas:
-                string list of meta to retrieve. Data series are sorted
-                by metas by default. If meta's name starts with '-',
-                then series are sorted in descending order.
-            metrics:
-               list of metrics to use.
-        plotter:
+        Every dictionary is a figure's description, containing
+        the following keys:
+
+        * *name*: string providing figure's name
+
+        * *series*: dictionary describing data required to draw the figure,
+          made of 2 keys:
+
+            * *metas*: string list of metas to retrieve.
+              Data series are sorted by metas by default.
+              If meta's name starts with '-',
+              then series are sorted in descending order.
+
+            * *metrics*: list of metrics to use.
+
+        * *plotter*:
             callable object that will be given metrics to plot
         """
         raise NotImplementedError  # pragma: no cover
