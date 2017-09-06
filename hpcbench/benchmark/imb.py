8387d34b16c6a636063ce5d62eed114c1314f717
Tristan Carel <tristan.carel@epfl.ch>
2017-09-06 15:49:28 +0200

    Add generic way to handle AlltoAallv and Allgather

--- hpcbench/benchmark/imb.py
+++ hpcbench/benchmark/imb.py
@@ -1,7 +1,7 @@
 """The Intel MPI Benchmarks
     https://software.intel.com/en-us/articles/intel-mpi-benchmarks
 """
-import os.path as osp
+from abc import abstractmethod
 import re
 
 from cached_property import cached_property
@@ -15,52 +15,65 @@ from hpcbench.toolbox.process import find_executable
 
 
 class IMBExtractor(MetricsExtractor):
-    """Ignore stdout until this line"""
-    STDOUT_IGNORE_PRIOR = (
-        "# Benchmarking PingPong"
-    )
-
-    latency_bandwidth = re.compile(
-        r'^\s*(\d)+\s+\d+\s+([0-9]*\.?[0-9]+)[\s]+([0-9]*\.?[0-9]+)'
-    )
-
-    METRICS = dict(
-        latency=Metrics.Second,
-        bandwidth=Metrics.Flops,
-    )
-
-    METRICS_NAMES = set(METRICS)
-
     @property
     def metrics(self):
         """ The metrics to be extracted.
             This property can not be replaced, but can be mutated as required
         """
-        return IMBExtractor.METRICS
+        return self.METRICS
 
     def extract(self, outdir, metas):
-        metrics = {}
-        s_latency = set()
-        s_bandwidth = set()
         # parse stdout and extract desired metrics
         with open(self.stdout(outdir)) as istr:
             for line in istr:
-                if line.strip() == IMBExtractor.STDOUT_IGNORE_PRIOR:
+                if line.strip() == self.STDOUT_IGNORE_PRIOR:
                     break
             for line in istr:
-                line = line.strip()
-                print(repr(line))
-                search = IMBExtractor.latency_bandwidth.search(line)
-                if search:
-                    byte = int(search.group(1))
-                    if byte != 0:
-                        s_latency.add(float(search.group(2)))
-                        s_bandwidth.add(float(search.group(3)))
+                self.process_line(line.strip())
+        return self.epilog()
 
-        metrics["latency"] = min(s_latency)
-        metrics["bandwidth"] = max(s_bandwidth)
+    @abstractmethod
+    def process_line(self):
+        """Process a line
+        """
+
+    @abstractmethod
+    def epilog(self):
+        """:return: extracted metrics as a dictionary
+        """
+
+
+class IMBPingPongExtractor(MetricsExtractor):
+    """Ignore stdout until this line"""
+    STDOUT_IGNORE_PRIOR = "# Benchmarking PingPong"
+    METRICS = dict(
+        latency=Metrics.Second,
+        bandwidth=Metrics.Flops,
+    )
+    METRICS_NAMES = set(METRICS)
+
+    LATENCY_BANDWIDTH = re.compile(
+        r'^\s*(\d)+\s+\d+\s+([0-9]*\.?[0-9]+)[\s]+([0-9]*\.?[0-9]+)'
+    )
+
+    def __init__(self):
+        super(IMBPingPongExtractor, self).__init__()
+        self.s_latency = set()
+        self.s_bandwidth = set()
+
+    def process_line(self, line):
+        search = self.LATENCY_BANDWIDTH.search(line)
+        if search:
+            byte = int(search.group(1))
+            if byte != 0:
+                self.s_latency.add(float(search.group(2)))
+                self.s_bandwidth.add(float(search.group(3)))
+
+    def epilog(self, metrics):
+        metrics["latency"] = min(self.s_latency)
+        metrics["bandwidth"] = max(self.s_bandwidth)
         # ensure all metrics have been extracted
-        unset_attributes = IMBExtractor.METRICS_NAMES - set(metrics)
+        unset_attributes = self.METRICS_NAMES - set(metrics)
         if any(unset_attributes):
             error = \
                 'Could not extract some metrics: %s\n' \
@@ -70,20 +83,67 @@ class IMBExtractor(MetricsExtractor):
         return metrics
 
 
+class IMBAllToAllExtractor(MetricsExtractor):
+    """Ignore stdout until this line"""
+    STDOUT_IGNORE_PRIOR = "# Benchmarking Allgather"
+    METRICS = dict(
+        # FIXME
+    )
+    METRICS_NAMES = set(METRICS)
+
+    def __init__(self):
+        super(IMBAllToAllExtractor, self).__init__()
+        # TODO TIM: define member variables if necessary
+
+    def process_line(self, line):
+        # TODO TIM: extract data from line
+        pass
+
+    def epilog(self):
+        # TODO TIM: return the metrics
+        pass
+
+
+class IMBAllGatherExtractor(MetricsExtractor):
+    """Ignore stdout until this line"""
+    STDOUT_IGNORE_PRIOR = "# Benchmarking Allgather"
+    METRICS = dict(
+        # FIXME
+    )
+    METRICS_NAMES = set(METRICS)
+
+    def __init__(self):
+        super(IMBAllGatherExtractor, self).__init__()
+        # TODO TIM: define member variables if necessary
+
+    def process_line(self, line):
+        # TODO TIM: extract data from line
+        pass
+
+    def epilog(self):
+        # TODO TIM: return the metrics
+        pass
+
+
 class IMB(Benchmark):
     """Benchmark wrapper for the IMBbench utility
     """
-    DEFAULT_THREADS = [1]
-    DEFAULT_DEVICE = 'cpu'
     DEFAULT_EXECUTABLE = 'IMB-MPI1'
+    PING_PONG = 'PingPong'
+    ALL_TO_ALL = 'Alltoallv'
+    ALL_GATHER = 'Allgather'
 
     def __init__(self):
         super(IMB, self).__init__(
             attributes=dict(
                 threads=IMB.DEFAULT_THREADS,
                 data="",
-                device=IMB.DEFAULT_DEVICE,
-                executable=IMB.DEFAULT_EXECUTABLE
+                executable=IMB.DEFAULT_EXECUTABLE,
+                categories=[
+                    IMB.PING_PONG,
+                    IMB.ALL_TO_ALL,
+                    IMB.ALL_GATHER,
+                ],
             )
         )
     name = 'imb'
@@ -98,11 +158,16 @@ class IMB(Benchmark):
 
     @property
     def execution_matrix(self):
-        yield dict(
-            category=IMB.DEFAULT_DEVICE,
-            command=[self.executable],
-        )
+        for category in self.attributes['categories']:
+            yield dict(
+                category=category,
+                command=[self.executable, category],
+            )
 
     @cached_property
     def metrics_extractors(self):
-        return IMBExtractor()
+        return {
+            IMB.PING_PONG: IMBPingPongExtractor(),
+            IMB.ALL_TO_ALL: IMBAllToAllExtractor(),
+            IMB.ALL_GATHER: IMBAllGatherExtractor(),
+        }
