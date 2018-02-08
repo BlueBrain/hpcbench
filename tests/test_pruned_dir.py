ce7a8b065a9b080fb2f01476a06d0bc768c7b399
Tristan Carel <tristan.carel@gmail.com>
2018-02-08 09:04:13 +0100

    Remove debug messages

--- tests/test_pruned_dir.py
+++ tests/test_pruned_dir.py
@@ -12,6 +12,4 @@ class PrunedDirTest(DriverTestCase, unittest.TestCase):
                            PrunedDirTest.driver.node, 'uc1')
         uc2path = osp.join(PrunedDirTest.CAMPAIGN_PATH,
                            PrunedDirTest.driver.node, 'uc2')
-        for root, dirs, files in os.walk(PrunedDirTest.CAMPAIGN_PATH):
-            print(root, dirs, files)
         self.assertTrue(osp.exists(uc1path) and not osp.exists(uc2path))
