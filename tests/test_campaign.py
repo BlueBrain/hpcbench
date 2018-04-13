91628d97665cb0ec1d7fd239dd4ae56423aba084
Tristan Carel <tristan.carel@gmail.com>
2018-04-13 15:29:11 +0200

    Rename campaign.fill_default_campaign_values to default_campaign

--- tests/test_campaign.py
+++ tests/test_campaign.py
@@ -9,7 +9,7 @@ import unittest
 import yaml
 
 from hpcbench.campaign import (
-    fill_default_campaign_values,
+    default_campaign,
     pip_installer_url,
 )
 from hpcbench.cli import bensh
@@ -20,7 +20,6 @@ from hpcbench.driver import (
     FixedAttempts,
     Top,
 )
-from hpcbench.toolbox.collections_ext import nameddict
 
 
 LOGGER = logging.getLogger()
@@ -50,7 +49,7 @@ class TestVersion(unittest.TestCase):
 class TestCampaign(unittest.TestCase):
     @property
     def new_config(self):
-        config = fill_default_campaign_values(nameddict())
+        config = default_campaign()
         config['network']['tags'] = dict(
             by_node=dict(
                 nodes=['node1', 'node2']
@@ -60,20 +59,20 @@ class TestCampaign(unittest.TestCase):
             ),
             by_nodeset=dict(
                 nodes='node[1-2]'
-            )
+            ),
         )
         return config
 
     def test_tags_re_conversion(self):
         RE_TYPE = type(re.compile('foo'))
-        config = fill_default_campaign_values(self.new_config)
+        config = default_campaign(self.new_config)
         self.assertIsInstance(
             config['network']['tags']['by_regex'][0]['match'],
             RE_TYPE
         )
 
     def test_nodeset(self):
-        config = fill_default_campaign_values(self.new_config)
+        config = default_campaign(self.new_config)
         self.assertEqual(
             config['network']['tags']['by_nodeset'][0]['nodes'],
             [
@@ -85,7 +84,7 @@ class TestCampaign(unittest.TestCase):
     def test_tags_tag_as_string(self):
         config = self.new_config
         config.network.tags['A'] = dict(tags='by_node')
-        filled_config = fill_default_campaign_values(config)
+        filled_config = default_campaign(config)
         self.assertEqual(
             filled_config.network.tags.A,
             [dict(nodes=['node1', 'node2'])]
@@ -98,7 +97,7 @@ class TestCampaign(unittest.TestCase):
             nodes=['node1']
         )
         with self.assertRaises(Exception) as exc:
-            fill_default_campaign_values(config)
+            default_campaign(config)
         self.assertTrue(exc.exception.message.startswith(
             "Tag 'A' is based on more than one criteria: "
         ))
@@ -110,14 +109,14 @@ class TestCampaign(unittest.TestCase):
         config['network']['tags']['B'] = dict(
             tags=['A'])
         with self.assertRaises(Exception):
-            fill_default_campaign_values(config)
+            default_campaign(config)
 
     def test_recursive_tag_missing(self):
         config = self.new_config
         config['network']['tags']['atag'] = dict(
             tags=['dontexist'])
         with self.assertRaises(Exception):
-            fill_default_campaign_values(config)
+            default_campaign(config)
 
     def test_tags_invalid_mode(self):
         config = self.new_config
@@ -125,19 +124,19 @@ class TestCampaign(unittest.TestCase):
             unknown=''
         )
         with self.assertRaises(Exception):
-            fill_default_campaign_values(config)
+            default_campaign(config)
 
     def test_envvars_expansion(self):
         with self.assertRaises(KeyError):
             config = self.new_config
             config['output_dir'] = "$OUTPUT_DIR"
-            fill_default_campaign_values(config)
+            default_campaign(config)
 
         os.environ['OUTPUT_DIR'] = 'output-dir'
         try:
             config = self.new_config
             config['output_dir'] = "$OUTPUT_DIR"
-            config = fill_default_campaign_values(config)
+            config = default_campaign(config)
             self.assertEqual(config['output_dir'], 'output-dir')
         finally:
             os.environ.pop('OUTPUT_DIR')
