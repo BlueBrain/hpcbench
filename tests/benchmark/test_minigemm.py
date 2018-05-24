b4352d23251ba2d95a440ec2721364928e3c7547
Omar Awile <omar.awile@epfl.ch>
2018-05-24 11:21:35 +0200

    fixed metrics extraction, compile options
    and added metrics extraction tests

---
+++ tests/benchmark/test_minigemm.py
@@ -0,0 +1,27 @@
+import unittest
+
+from hpcbench.benchmark.minigemm import MiniGEMM
+from . benchmark import AbstractBenchmarkTest
+
+
+class TestMiniGEMM(AbstractBenchmarkTest, unittest.TestCase):
+    EXPECTED_METRICS = dict(
+        time=135.999,
+        gflops=198.926,
+        checksum=1.48311e+20,
+    )
+
+    def get_benchmark_clazz(self):
+        return MiniGEMM
+
+    def get_expected_metrics(self, category):
+        return TestMiniGEMM.EXPECTED_METRICS
+
+    def get_benchmark_categories(self):
+        return [self.get_benchmark_clazz().name]
+
+    @property
+    def attributes(self):
+        return dict(
+            executable='/path/to/fake'
+        )
