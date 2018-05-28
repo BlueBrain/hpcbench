fa4ab46b8553e2565c469fed67310d7d48b26c5e
Tristan Carel <tristan.carel@gmail.com>
2018-05-28 09:56:22 +0200

    Update benchmark `modules` and `environment` overriding mechanism
    * fined-grain environment overriding
    * refactor test case to tackle all use-cases
    
    keyCreate test benchmark

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,30 +1,102 @@
+from __future__ import print_function
+
+import contextlib
 import glob
 import os.path as osp
-from textwrap import dedent
 import unittest
 
+from cached_property import cached_property
+import six
+import yaml
+
+from hpcbench.api import Benchmark
 from hpcbench.campaign import ReportNode
-from . import DriverTestCase
+from . import DriverTestCase, NullExtractor
+
+
+class EnvBenchmark(Benchmark):
+    name = "environment"
+    description = "only for testing purpose"
+
+    def __init__(self):
+        super(EnvBenchmark, self).__init__(
+            attributes=dict(
+                environment={},
+                modules=[]
+            )
+        )
+
+    @property
+    def metric_required(self):
+        return False
+
+    @property
+    def metrics_extractors(self):
+        return NullExtractor()
+
+    @property
+    def environment(self):
+        return self.attributes['environment']
+
+    @property
+    def modules(self):
+        return self.attributes['modules']
+
+    def execution_matrix(self, context):
+        yield dict(
+            category='ut',
+            command=['true'],
+            environment=self.environment,
+            modules=self.modules
+        )
 
 
 class TestEnvironment(DriverTestCase, unittest.TestCase):
-    EXPECTED_SHELL_SCRIPT_PRELUDE = dedent("""\
-        #!/bin/sh
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
-        module purge
-        module load foo/bar
-        export foo=bar
-        """)
+    def _shell_prelude(self, environment=None, modules=None):
+        """Rebuild shell-script prelude to ensure that
+        both environment variables and modules are properly
+        set.
+        """
+        environment = environment or {}
+        modules = modules or []
+
+        ostr = six.StringIO()
+        print('#!/bin/sh', file=ostr)
+        print(file=ostr)
+        print('module purge', file=ostr)
+        for module in modules:
+            print('module load ' + module, file=ostr)
+        for name, value in environment.items():
+            print('export', name + '=' + value, file=ostr)
+        with contextlib.closing(ostr):
+            return ostr.getvalue()
+
+    @cached_property
+    def expected_results(self):
+        with open(self.get_campaign_file()) as istr:
+            return yaml.load(istr)['expected_results']
 
     def test(self):
         report = ReportNode(self.CAMPAIGN_PATH)
-        self.assertEqual(
-            report.collect_one('environment'),
-            dict(foo='bar')
-        )
-        path, modules = report.collect_one('modules', with_path=True)
-        self.assertEqual(modules, ['foo/bar'])
-        shell_script = glob.glob(osp.join(path, '*.sh'))[0]
-        with open(shell_script) as istr:
-            expected = self.EXPECTED_SHELL_SCRIPT_PRELUDE
-            self.assertTrue(istr.read().startswith(expected))
+        for path, results in report.collect('modules', 'environment',
+                                            with_path=True):
+            context = report.path_context(path)
+            self.assertIn(context.benchmark, self.expected_results)
+            expected_results = self.expected_results[context.benchmark]
+            self.assertEqual(
+                expected_results['modules'],
+                results[0],
+            )
+            self.assertEqual(
+                expected_results['environment'],
+                results[1],
+            )
+            shell_script = glob.glob(osp.join(path, '*.sh'))[0]
+            with open(shell_script) as istr:
+                prelude = self._shell_prelude(**expected_results)
+                script = istr.read()
+                self.assertTrue(script.startswith(prelude))
